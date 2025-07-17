f36fc9e50fa6973e67fc8df9e611a41c7964ea94
Dr Griffith Rees <grees@turing.ac.uk>
2025-07-17 05:36:26 +0100

    fix: cruft in lightning_gpt/callbacks.py

--- lightning_gpt/callbacks.py
+++ lightning_gpt/callbacks.py
@@ -16,23 +16,29 @@ class CPUMetricsCallback(Callback):
 
     def on_train_epoch_start(self, trainer: Trainer, pl_module: LightningModule) -> None:
         self.start_time = time.time()
+        self._epoch_losses: list[list[float]] = []
 
     def on_train_epoch_end(self, trainer: "Trainer", pl_module: "LightningModule") -> None:
-        import ipdb
-
-        ipdb.set_trace()
+        """
+        Note:
+            Below has NOT been tested and is derived from CUDA and likely
+            unsupported for CPU.  The methods like `root_gpu` are likely 
+            to fail.
+        """
         self._torch_runner.synchronize(self.root_gpu(trainer))
         max_memory: float = self._torch_runner.max_memory_allocated(self.root_gpu(trainer)) / 2**20
         epoch_time: float = time.time() - self.start_time
-        loss: float = self._torch_runner.loss()
+        self._epoch_losses.append(copy(pl_module._losses))
+        pl_module._losses.clear()
+        loss_mean: float = mean(self._epoch_losses[-1])
 
         max_memory = trainer.strategy.reduce(max_memory)
         epoch_time = trainer.strategy.reduce(epoch_time)
+        loss_mean_means: float = trainer.strategy.reduce(loss_mean)
 
         rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
         rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
-        rank_zero_info(f"Loss: {loss:.2f}")
-        assert False
+        rank_zero_info(f"Loss: {loss_mean_means:.2f}")
 
     def root_gpu(self, trainer: "Trainer") -> int:
         return trainer.strategy.root_device.index
@@ -55,6 +61,9 @@ class CUDAMetricsCallback(Callback):
         self._torch_runner.synchronize(self.root_gpu(trainer))
         max_memory: float = self._torch_runner.max_memory_allocated(self.root_gpu(trainer)) / 2**20
         epoch_time: float = time.time() - self.start_time
+        # For debugging uncomment the following
+        # from pprint import pprint
+        # pprint(pl_module._losses)
         self._epoch_losses.append(copy(pl_module._losses))
         pl_module._losses.clear()
         loss_mean: float = mean(self._epoch_losses[-1])
@@ -79,23 +88,3 @@ class XPUMetricsCallback(CUDAMetricsCallback):
     @property
     def _torch_runner(self):
         return torch.xpu
-
-    # def on_train_epoch_start(self, trainer: "Trainer", pl_module: "LightningModule") -> None:
-    #     # Reset the memory use counter
-    #     torch.xpu.reset_peak_memory_stats(self.root_gpu(trainer))
-    #     torch.xpu.synchronize(self.root_gpu(trainer))
-    #     self.start_time = time.time()
-
-    # def on_train_epoch_end(self, trainer: "Trainer", pl_module: "LightningModule") -> None:
-    #     torch.xpu.synchronize(self.root_gpu(trainer))
-    #     max_memory = torch.xpu.max_memory_allocated(self.root_gpu(trainer)) / 2**20
-    #     epoch_time = time.time() - self.start_time
-    #
-    #     max_memory = trainer.strategy.reduce(max_memory)
-    #     epoch_time = trainer.strategy.reduce(epoch_time)
-    #
-    #     rank_zero_info(f"Average Epoch time: {epoch_time:.2f} seconds")
-    #     rank_zero_info(f"Average Peak memory {max_memory:.2f}MiB")
-    #
-    # def root_gpu(self, trainer: "Trainer") -> int:
-    #     return trainer.strategy.root_device.index
